8746ff1f6296b9ff6359c109ec47af84d21e5d4b
Roy Le <roy.le@viindoo.com>
2023-11-18 11:55:27 +0700

    [FIX] project: fill default project_task_user_rel stage_id

--- openupgrade_scripts/scripts/project/16.0.1.2/post-migration.py
+++ openupgrade_scripts/scripts/project/16.0.1.2/post-migration.py
@@ -5,7 +5,20 @@ def active_group_project_milestone(env):
     env["res.config.settings"].create({"group_project_milestone": True}).execute()
 
 
+def _fill_default_project_task_user_rel_stage_id(env):
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE project_task_user_rel rel
+            SET stage_id = typ.id
+        FROM project_task_type typ
+        WHERE rel.stage_id IS NULL AND typ.user_id = rel.user_id
+        """,
+    )
+
+
 @openupgrade.migrate()
 def migrate(env, version):
     active_group_project_milestone(env)
     openupgrade.load_data(env.cr, "project", "16.0.1.2/noupdate_changes.xml")
+    _fill_default_project_task_user_rel_stage_id(env)
