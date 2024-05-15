4faf355f771357a8dafe8d6b2d1c3cecfb989c38
Roy Le <roy.le@viindoo.com>
2024-05-15 18:20:38 +0700

    [MIG] auth_oauth, auth_signup, auth_totp_mail: migrate to 17.0

---
+++ openupgrade_scripts/scripts/auth_signup/17.0.1.0/pre-migration.py
@@ -0,0 +1,11 @@
+# Copyright 2024 Viindoo Technology Joint Stock Company (Viindoo)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.delete_records_safely_by_xml_id(
+        env,
+        ["auth_signup.reset_password_email"],
+    )
