4243b30d21a9886390a965b668f9e103884e857a
Roy Le <roy.le@viindoo.com>
2024-05-16 11:02:41 +0700

    [IMP] auth_signup: renamed in pre-migration and removed in post-migration to avoid model has not been loaded

--- openupgrade_scripts/scripts/auth_signup/17.0.1.0/post-migration.py
+++ openupgrade_scripts/scripts/auth_signup/17.0.1.0/post-migration.py
@@ -7,3 +7,7 @@ from openupgradelib import openupgrade
 @openupgrade.migrate()
 def migrate(env, version):
     openupgrade.load_data(env, "auth_signup", "17.0.1.0/noupdate_changes.xml")
+    openupgrade.delete_records_safely_by_xml_id(
+        env,
+        ["auth_signup.openupgrade_legacy_17_0_reset_password_email"],
+    )
--- openupgrade_scripts/scripts/auth_signup/17.0.1.0/pre-migration.py
+++ openupgrade_scripts/scripts/auth_signup/17.0.1.0/pre-migration.py
@@ -2,10 +2,14 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from openupgradelib import openupgrade
 
+_xmlids_renames = [
+    (
+        "auth_signup.reset_password_email",
+        "auth_signup.openupgrade_legacy_17_0_reset_password_email",
+    ),
+]
+
 
 @openupgrade.migrate()
 def migrate(env, version):
-    openupgrade.delete_records_safely_by_xml_id(
-        env,
-        ["auth_signup.reset_password_email"],
-    )
+    openupgrade.rename_xmlids(env.cr, _xmlids_renames)
