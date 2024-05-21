ae511f2f5dad04dfa10144c6ce71ced0132f9bf2
Roy Le <roy.le@viindoo.com>
2024-05-21 13:50:38 +0700

    [OU-IMP] *: Reflect updated analysis in work file
    Nothing extra to be done, but for having both files synced.

--- openupgrade_scripts/scripts/rating/17.0.1.1/pre-migration.py
+++
@@ -1,8 +0,0 @@
-from openupgradelib import openupgrade
-
-
-@openupgrade.migrate()
-def migrate(env, version):
-    openupgrade.delete_sql_constraint_safely(
-        env, "rating", "rating_rating", "rating_range"
-    )
--- openupgrade_scripts/scripts/utm/17.0.1.1/pre-migration.py
+++
@@ -1,9 +0,0 @@
-from openupgradelib import openupgrade
-
-
-@openupgrade.migrate()
-def migrate(env, version):
-    openupgrade.delete_sql_constraint_safely(env, "utm", "utm_campaign", "unique_name")
-    openupgrade.delete_sql_constraint_safely(env, "utm", "utm_medium", "unique_name")
-    openupgrade.delete_sql_constraint_safely(env, "utm", "utm_source", "unique_name")
-    openupgrade.delete_sql_constraint_safely(env, "utm", "utm_tag", "name_uniq")
