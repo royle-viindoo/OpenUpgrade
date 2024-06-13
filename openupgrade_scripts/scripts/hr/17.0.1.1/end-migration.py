b4008f9880c3e0087f1b5cb0df8450d25d7095eb
Nguyễn Đại Dương <daiduongnguyen2709@gmail.com>
2024-06-13 13:55:38 +0700

    [OU-ADD] hr: migration to 17.0

---
+++ openupgrade_scripts/scripts/hr/17.0.1.1/end-migration.py
@@ -0,0 +1,28 @@
+# Copyright 2024 Viindoo Technology Joint Stock Company (Viindoo)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from openupgradelib import openupgrade
+
+
+def _merge_employee_contact(env):
+    """
+    Performing merge 'address_home_id' into 'work_contact_id'
+    """
+    partner_merge_wizard = env["base.partner.merge.automatic.wizard"]
+    partner = env["res.partner"]
+    env.cr.execute(
+        """
+        SELECT address_home_id, work_contact_id
+        FROM hr_employee
+        WHERE work_contact_id IS NOT NULL AND address_home_id IS NOT NULL
+        AND work_contact_id<>address_home_id
+        """
+    )
+    for address_home_id, work_contact_id in env.cr.fetchall():
+        partner_merge_wizard._merge(
+            [address_home_id, work_contact_id], partner.browse(work_contact_id)
+        )
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    _merge_employee_contact(env)
