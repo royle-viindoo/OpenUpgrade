651828d307e31d2ae084a135252b2f29d2d89df3
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2025-03-11 20:47:45 +0100

    [OU-FIX] hr_recruitment: Ensure order is preserved
    The only way to do it is to set the sequence with the old order
    criteria: the id.
    
    TT50074

--- openupgrade_scripts/scripts/hr_recruitment/17.0.1.1/pre-migration.py
+++ openupgrade_scripts/scripts/hr_recruitment/17.0.1.1/pre-migration.py
@@ -1,4 +1,5 @@
 # Copyright 2025 Le Filament (https://le-filament.com)
+# Copyright 2025 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from openupgradelib import openupgrade
 
@@ -6,17 +7,9 @@ from openupgradelib import openupgrade
 def _fill_sequence_hr_applicant_refuse_reason(cr):
     openupgrade.logged_query(
         cr,
-        """
-        ALTER TABLE hr_applicant_refuse_reason
-            ADD COLUMN IF NOT EXISTS sequence INTEGER DEFAULT 10;
-        """,
+        "ALTER TABLE hr_applicant_refuse_reason ADD COLUMN IF NOT EXISTS sequence int4",
     )
-    openupgrade.logged_query(
-        cr,
-        """
-        ALTER TABLE hr_applicant_refuse_reason ALTER COLUMN sequence DROP DEFAULT;
-        """,
-    )
+    openupgrade.logged_query(cr, "UPDATE hr_applicant_refuse_reason SET sequence=id")
 
 
 @openupgrade.migrate(use_env=False)
