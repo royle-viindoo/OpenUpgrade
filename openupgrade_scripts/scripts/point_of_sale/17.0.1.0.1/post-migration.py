b7206015f29bcb6092ef203b7446d135ba109f80
Roy Le <roy.le@viindoo.com>
2025-03-20 09:15:58 +0700

    [MIG] point_of_sale: migrate for 17.0

--- openupgrade_scripts/scripts/point_of_sale/17.0.1.0.1/post-migration.py
+++ openupgrade_scripts/scripts/point_of_sale/17.0.1.0.1/post-migration.py
@@ -3,7 +3,16 @@
 
 from openupgradelib import openupgrade
 
+_deleted_xml_records = [
+    "point_of_sale.rule_pos_account_move",
+    "point_of_sale.rule_pos_account_move_line",
+]
+
 
 @openupgrade.migrate()
 def migrate(env, version):
     openupgrade.load_data(env, "point_of_sale", "17.0.1.0.1/noupdate_changes.xml")
+    openupgrade.delete_records_safely_by_xml_id(
+        env,
+        _deleted_xml_records,
+    )
--- openupgrade_scripts/scripts/point_of_sale/17.0.1.0.1/pre-migration.py
+++ openupgrade_scripts/scripts/point_of_sale/17.0.1.0.1/pre-migration.py
@@ -2,12 +2,16 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from openupgradelib import openupgrade
 
+_models_renames = [
+    ("restaurant.printer", "pos.printer"),
+]
+
 _tables_renames = [
-    ("pos_config_printer_rel", "openupgrade_legacy_17_0_pos_config_printer_rel"),
-    ("printer_category_rel", "openupgrade_legacy_17_0_printer_category_rel"),
+    ("restaurant_printer", "pos_printer"),
 ]
 
 
 @openupgrade.migrate()
 def migrate(env, version):
+    openupgrade.rename_models(env.cr, _models_renames)
     openupgrade.rename_tables(env.cr, _tables_renames)
