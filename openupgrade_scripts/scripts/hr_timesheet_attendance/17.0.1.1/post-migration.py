2eb80a644263a8702e1b2c661c6d8fb6026f0bb7
Roy Le <roy.le@viindoo.com>
2025-03-20 08:17:16 +0700

    [MISC] hr_timesheet_attendance: Line too long

--- openupgrade_scripts/scripts/hr_timesheet_attendance/17.0.1.1/post-migration.py
+++ openupgrade_scripts/scripts/hr_timesheet_attendance/17.0.1.1/post-migration.py
@@ -6,4 +6,6 @@ from openupgradelib import openupgrade
 
 @openupgrade.migrate()
 def migrate(env, version):
-    openupgrade.load_data(env, "hr_timesheet_attendance", "17.0.1.1/noupdate_changes.xml")
+    openupgrade.load_data(
+        env, "hr_timesheet_attendance", "17.0.1.1/noupdate_changes.xml"
+    )
